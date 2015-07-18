b846aace61ea45d5ecda5069d7c7f51d0fe1ccd2
Sylvain <lbsylvain1@aol.com>
2015-07-18 15:07:04 +1100

    setup project

--- static/app/config.js
+++ static/app/config.js
@@ -17,8 +17,8 @@ System.config({
   "map": {
     "angular": "github:angular/bower-angular@1.4.2",
     "angular-ui-router": "github:angular-ui/ui-router@0.2.15",
-    "babel": "npm:babel-core@5.6.17",
-    "babel-runtime": "npm:babel-runtime@5.6.17",
+    "babel": "npm:babel-core@5.7.4",
+    "babel-runtime": "npm:babel-runtime@5.7.0",
     "core-js": "npm:core-js@0.9.18",
     "jquery": "github:components/jquery@2.1.4",
     "github:angular-ui/ui-router@0.2.15": {
@@ -28,6 +28,9 @@ System.config({
       "process": "npm:process@0.10.1"
     },
     "npm:babel-runtime@5.6.17": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:babel-runtime@5.7.0": {
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
     "npm:core-js@0.9.18": {
